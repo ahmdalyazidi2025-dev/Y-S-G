c76308cc27d997aa1d6a04f643aff6e59022e17f
Ahmed Alyazidi <ahmd.alyazidi2025@gmail.com>
2026-02-22 04:46:51 +0300

    chore: remove test files

--- test-pwd-reset.ts
+++
@@ -1,9 +0,0 @@
-import { requestPasswordResetAction } from "./src/app/actions/auth-actions"
-
-async function test() {
-    console.log("Testing requestPasswordResetAction...")
-    const res = await requestPasswordResetAction("0555555555")
-    console.log("Result:", res)
-}
-
-test()
